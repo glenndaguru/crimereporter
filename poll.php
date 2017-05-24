a2d9d95236ef8544d287733564c636a09a204c7b
glenndaguru <glenn.masango101@gmail.com>
2017-05-24 10:09:47 +0200

    Scripts added Signed-off-by: glenndaguru <glenn.masango101@gmail.com>

---
+++ poll.php
@@ -0,0 +1,38 @@
+<?php
+	// Data From App
+	$userEmail = $_POST["userEmail"];
+	$userPass = $_POST["userPass"];
+
+	// Connection
+	$servername = "127.4.48.2";
+	$username = "adminD35M7Lk";
+	$password = "M1iBd32D8Hdt";
+	$dbname = "crimereporter";
+	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+		
+	if ($conn->connect_error) 
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	// Selecting User ID, check if user exists
+	$sql = "SELECT UserID FROM User WHERE userEmail='".$userEmail."' AND userPass='".$userPass."'";
+	$result = mysqli_query($conn,$sql) or die("Error in $sql:" . mysqli_error($conn));	
+	if(mysqli_num_rows($result) > 0)
+	{
+		$myObj->result = 1;
+	}
+	else
+	{
+		$myObj->result = 0;
+	}
+	
+	$myJobj = json_encode($myObj);
+	echo $myJobj."\n";
+	
+	mysqli_close($conn);
+	
+
+?>
---
+++ register.php
@@ -0,0 +1,37 @@
+<?php
+	// Data From App
+	$userName = $_POST["userName"];
+	$userEmail = $_POST["userEmail"];
+	$userPass = $_POST["userPass"];
+	$userNo = $_POST["userNo"];
+
+	// Connection
+	$servername = "127.4.48.2";
+	$username = "adminD35M7Lk";
+	$password = "M1iBd32D8Hdt";
+	$dbname = "crimereporter";
+		
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	if ($conn->connect_error) 
+	{
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	
+	// Insert Into User Table
+	$sql = 'INSERT INTO User'.'(userName, userEmail, userPass, userNo)'.'VALUES ("'.$userName.'", "'.$userEmail.'","'.$userPass.'","'.$userNo.'")';
+	if (!mysqli_query($conn, $sql)) 
+	{
+		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	}
+	else
+	{
+		$myObj->result = "User succesfully registered";
+	}
+	
+	$myJobj = json_encode($myObj);
+	echo $myJobj."\n";
+	
+	mysqli_close($conn);
+?>
