33fbf215466b8d0efa711d26690ed5fc063fa08c
glenndaguru <glenn.masango101@gmail.com>
2017-06-12 09:27:04 +0200

    testing.php
    Signed-off-by: glenndaguru <glenn.masango101@gmail.com>

---
+++ testing.php
@@ -0,0 +1,20 @@
+<?php
+
+	// Connection
+	$servername = "127.4.48.2";
+	$username = "adminD35M7Lk";
+	$password = "M1iBd32D8Hdt";
+	$dbname = "evoting";
+	
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+		
+	if ($conn->connect_error) 
+	{
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	
+	mysqli_close($conn);
+	
+?>
