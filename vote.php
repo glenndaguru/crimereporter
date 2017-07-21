e2125c29188fbec520ea981d90b2dd47949c091d
origin <glenn.masango101@gmail.com>
2017-07-21 11:32:33 +0200

    Here

--- vote.php
+++ vote.php
@@ -11,6 +11,7 @@
 	//Party Data
 	global $partyVotes;
 	global $theDate;
+	global $conn;
 
 	// Connection
 	$servername = "127.6.180.130";
@@ -19,51 +20,51 @@
 	$dbname = "evoting";
 		
 	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	$GLOBALS['conn'] = new mysqli($servername, $username, $password, $dbname);
 	
-	if ($conn->connect_error) 
+	if ($GLOBALS['conn']->connect_error) 
 	{
-		die("Connection failed: " . $conn->connect_error);
+		die("Connection failed: " . $GLOBALS['conn']->connect_error);
 	} 
 	
 	//Check if user has voted. (Check status on user table)
 	//If yes then don't allow to vote else allow to vote. (Update status on user Table,  Get the current value of the votes and add the value)
 	$sql = "SELECT userStatus FROM Users WHERE userIDNo='".md5($userIDNo)."' AND userPass='".md5($userPass)."'";
-	$result = mysqli_query($conn,$sql) or die("Error in $sql:" . mysqli_error($conn));	
+	$result = mysqli_query($GLOBALS['conn'],$sql) or die("Error in $sql:" . mysqli_error($GLOBALS['conn']));	
 	while($row = mysqli_fetch_object($result))
 	{
 		$GLOBALS['userStatus'] = $row->userStatus;
 	} 
 	
 	$sql = "SELECT partyVotes FROM User_Vote WHERE partyID='".$userCandi."'";
-	$result = mysqli_query($conn,$sql) or die("Error in $sql:" . mysqli_error($conn));	
+	$result = mysqli_query($GLOBALS['conn'],$sql) or die("Error in $sql:" . mysqli_error($GLOBALS['conn']));	
 	while($row = mysqli_fetch_object($result))
 	{
 		$GLOBALS['partyVotes'] = $row->partyVotes;
 	} 
 	
-	if($GLOBALS['userStatus'] = 0)
+	if($GLOBALS['userStatus'] == 0)
 	{
 		//calculate value 
 		$GLOBALS['partyVotes'] = $GLOBALS['partyVotes'] + 1;
 		
 		//set the date
-		$GLOBALS['theDate'] = date("Y/m/d");
+		$GLOBALS['theDate'] = date("Y-m-d");
 		
-		//$mysql = "UPDATE USERFILE SET userLastReplaced ='".$replaced_date."' WHERE file_path ='".$file_name."'AND file_name='".basename($file_name)."'AND user_email='".$GLOBALS['user_email']."'AND ip_address='".$GLOBALS['web_address']."'";
+		$GLOBALS['userStatus'] = 1;
 		
 		//Update User Table
-		$sql = "UPDATE Users SET userStatus='".$GLOBALS['userStatus']."' WHERE userIDNo='".md5($userIDNo)."' AND userPass='".md5($userPass)."'";
-		if (!mysqli_query($conn, $sql)) 
+		$sql_user = "UPDATE Users SET userStatus='".$GLOBALS['userStatus']."' WHERE userIDNo='".md5($userIDNo)."' AND userPass='".md5($userPass)."'";
+		if (!mysqli_query($GLOBALS['conn'], $sql_user)) 
 		{
-			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			echo "Error: " . $sql_user . "<br>" . mysqli_error($GLOBALS['conn']);
 		}	
 		
 		// Update Vote Table
-		$sql = "UPDATE User_Vote SET partyVotes='".$GLOBALS['partyVotes']."', voteDate='".$GLOBALS['partyVotes']."' WHERE partyID='".$userCandi."'";
-		if (!mysqli_query($conn, $sql)) 
+		$sql_vote = "UPDATE User_Vote SET partyVotes='".$GLOBALS['partyVotes']."', voteDate='".$GLOBALS['theDate']."' WHERE partyID='".$userCandi."'";
+		if (!mysqli_query($GLOBALS['conn'], $sql_vote)) 
 		{
-			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			echo "Error: " .$sql_vote. "<br>" . mysqli_error($GLOBALS['conn']);
 		}
 		else
 		{
@@ -78,6 +79,6 @@
 	$myJobj = json_encode($myObj);
 	echo $myJobj."\n";
 	
-	mysqli_close($conn);
+	mysqli_close($GLOBALS['conn']);
 
 ?>
