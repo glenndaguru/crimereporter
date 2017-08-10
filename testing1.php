adfe16cfee2d455a7cec10361fc46c9ef82234ca
origin <glenn.masango101@gmail.com>
2017-08-10 09:59:49 +0200

    testing1.php

--- testing1.php
+++ testing1.php
@@ -15,11 +15,11 @@
 	} 
 	
 	//Calculate the number of registered voters and return a %
-	$sql = "SELECT count(user_id) FROM Users";
+	$sql = "SELECT count(user_id) AS Voters FROM Users";
 	$result = mysqli_query($GLOBALS['conn'],$sql) or die("Error in $sql:" . mysqli_error($GLOBALS['conn']));	
 	$data= mysql_fetch_assoc($result);
 	
-	$myObj->result = $data;
+	$myObj->result = $data['Voters'];
 	
 	$myJobj = json_encode($myObj);
 	echo $myJobj."\n";
