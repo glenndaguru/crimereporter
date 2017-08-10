150e23506110bf4ae44208dd275ffb0a74873202
origin <glenn.masango101@gmail.com>
2017-08-10 10:08:39 +0200

    testing1.php

--- testing1.php
+++ testing1.php
@@ -16,8 +16,8 @@
 		die("Connection failed: " . $GLOBALS['conn']->connect_error);
 	} 
 	
-	//Calculate the number of registered voters and return a %
-	$sql = "SELECT count(user_id) AS Voters FROM Users";
+	//Calculate the number of registered voters
+	$sql = "SELECT count(user_id) AS Voters FROM Users WHERE userStatus = 1";
 	$result = mysqli_query($GLOBALS['conn'],$sql) or die("Error in $sql:" . mysqli_error($GLOBALS['conn']));	
 	$data= mysqli_fetch_assoc($result);
 	
